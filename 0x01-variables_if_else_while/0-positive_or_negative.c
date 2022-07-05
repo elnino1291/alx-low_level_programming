394f017d335b996b030ad23d4541ec5569235448
ekwueme anene anthony <aneneanthony50@gmail.com>
2022-07-05 05:12:09 -0700

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stido.h>
+#include <time.h>
+#include <stdlib.h>
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		pfintf("%d is negative\n", n):
+	return (0);
+}
+
