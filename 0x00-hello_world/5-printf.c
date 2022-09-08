15ff6efbffd3a0fdb4becae1230e7b5f1791c255
elnino1291 <aneneanthony50@gmail.com>
2022-09-08 05:29:17 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
-* main - "prints with proper grammar"
-*print the phrase "but the outcome is a piece of art"
+* main - prints the string in the put function
+*
+*Description: using the main function 
+*this program prints "programing is like buliding a multillgual puzzle
 *Return 0
 */
 
 
-	int main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
