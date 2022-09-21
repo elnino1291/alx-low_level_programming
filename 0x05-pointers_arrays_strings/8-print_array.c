1572e7de681930348ec3438c7a2b79db2e067a86
elnino1291 <aneneanthony50@gmail.com>
2022-09-21 03:55:01 -0700

    tasks

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,12 +12,12 @@ void print_array(int *a, int n)
 {
 	int inc;
 
-	for (inc = 0; a < n ; inc++)
+	for (inc = 0; inc < n ; inc++)
 	{
 		if (inc != n - 1)
 			print("%d, ", a[inc]);
 		else
-			print("%d, a[inc]);
+			print("%d", a[inc]);
 	}
 	putchar(10);
 }
